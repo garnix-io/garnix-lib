8d37d846a98e7abd23a714ed4aab837f17fa3202
Julian K. Arni <jkarni@gmail.com>
2024-06-10 12:19:05 +0200

    Create garnix.persistence module

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
 
   outputs = { self, nixpkgs }: {
 
+    nixosModules.garnix = { lib, config, ... }:
+    let cfg = config.garnix.persistence;
+    in {
+
+      options.garnix.persistence = {
+        enable = lib.mkEnableOption "Enable persistence in garnix deploys";
+
+        name = lib.mkOption {
+          type = lib.types.str;
+          description = "A unique name to identify this persistence." ;
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        users.users.garnix = {
+          description = "A user garnix uses for redeploying";
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTPguYAuqp6qCU43u8g2hgWz4MLCEPPyoVPYO53qB+t garnixServer@garnix.io"
+          ];
+          extraGroups = [ "wheel" ];
+        };
+      };
+    };
+
     lib = {
       getHashSubdomain = nixosCfg :
         let prefixLength = nixpkgs.lib.stringLength "/nix/store/";
