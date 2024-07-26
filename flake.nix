fe27e4e115e5cf63de9f5c856c77ad791f9f366d
Evie Ciobanu <me@eevie.ro>
2024-07-26 16:19:59 +0300

    address review comments

--- flake.nix
+++ flake.nix
@@ -9,70 +9,93 @@
 
     nixosModules.default = { lib, config, ... }:
       let
-        cfg = config.garnix.persistence;
+        cfg = config.garnix.server;
       in
       {
+        options.garnix = {
+          server = {
+            enable = lib.mkEnableOption "Turn on default settings for servers running on Garnix hardware.";
 
-        options.garnix.persistence = {
-          enable = lib.mkEnableOption "Turn this machine into a persistent machine in garnix deploys";
+            persistence = {
+              enable = lib.mkEnableOption "Turn this machine into a persistent machine in garnix deploys";
 
-          name = lib.mkOption {
-            type = lib.types.str;
-            description = "A unique name to identify this persistent nixos configuration. If a subsequent deploy defines a server with the same persistence name, it'll reuse the same machine, including disks.";
+              name = lib.mkOption {
+                type = lib.types.str;
+                description = "A unique name to identify this persistent nixos configuration. If a subsequent deploy defines a server with the same persistence name, it'll reuse the same machine, including disks.";
+              };
+            };
           };
         };
 
-        config = lib.mkIf cfg.enable {
-          assertions = [
-            {
-              assertion = config.security.sudo.enable;
-              message = "garnix.persistence needs security.sudo enabled, but some other module forced the value to 'false'";
-            }
-            {
-              assertion = config.security.sudo.execWheelOnly;
-              message = "garnix.persistence needs security.sudo.execWheelOnly enabled, but some other module forced the value to 'false'";
-            }
-            {
-              assertion = !config.security.sudo.wheelNeedsPassword;
-              message = "garnix.persistence needs security.sudo.wheelNeedsPassword disabled, but some other module forced the value to 'true'";
-            }
-            {
-              assertion = lib.elem "garnix" config.nix.settings.trusted-users;
-              message = "garnix.persistence needs the user 'garnix' to be present in 'nix.settings.trusted-users', but some other module forced it out.";
-            }
-            {
-              assertion = lib.elem "garnix" (lib.attrNames config.users.users);
-              message = "garnix.persistence needs the user 'garnix' to be present in 'users.users', but some other module forced it out.";
-            }
-          ];
-
-
-          fileSystems."/" = {
-            device = "/dev/sda1";
-            fsType = "ext4";
-          };
-          boot.loader.grub.device = "/dev/sda";
-
-          services.openssh.enable = true;
-
-          security.sudo = {
-            enable = true;
-            execWheelOnly = true;
-            wheelNeedsPassword = false;
-          };
-
-          nix.settings.trusted-users = [ "garnix" ];
-
-          users.users.garnix = {
-            description = "A user garnix uses for redeploying ${cfg.name}";
-            isNormalUser = true;
-            createHome = false;
-            openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTPguYAuqp6qCU43u8g2hgWz4MLCEPPyoVPYO53qB+t garnixServer@garnix.io"
+        config = lib.mkMerge [
+          (lib.mkIf cfg.enable {
+            assertions = [
+              {
+                assertion = config.fileSystems."/".device == "/dev/sda1";
+                message = "garnix.server needs the fileSystems.\"/\".device to be \"/dev/sda1\"";
+              }
+              {
+                assertion = config.fileSystems."/".fsType == "ext4";
+                message = "garnix.server needs the fileSystems.\"/\".fsType to be \"ext4\"";
+              }
+              {
+                assertion = config.boot.loader.grub.derice == "/dev/sda";
+                message = "garnix.server needs the fileSystems.\"/\".fsType to be \"ext4\"";
+              }
             ];
-            extraGroups = [ "wheel" ];
-          };
-        };
+
+            fileSystems."/" = {
+              device = "/dev/sda1";
+              fsType = "ext4";
+            };
+            boot.loader.grub.device = "/dev/sda";
+          })
+
+          (lib.mkIf (cfg.enable && cfg.persistence.enable) {
+            assertions = [
+              {
+                assertion = config.security.sudo.enable;
+                message = "garnix.server.persistence needs security.sudo enabled, but some other module forced the value to 'false'";
+              }
+              {
+                assertion = config.security.sudo.execWheelOnly;
+                message = "garnix.server.persistence needs security.sudo.execWheelOnly enabled, but some other module forced the value to 'false'";
+              }
+              {
+                assertion = !config.security.sudo.wheelNeedsPassword;
+                message = "garnix.server.persistence needs security.sudo.wheelNeedsPassword disabled, but some other module forced the value to 'true'";
+              }
+              {
+                assertion = lib.elem "garnix" config.nix.settings.trusted-users;
+                message = "garnix.server.persistence needs the user 'garnix' to be present in 'nix.settings.trusted-users', but some other module forced it out.";
+              }
+              {
+                assertion = lib.elem "garnix" (lib.attrNames config.users.users);
+                message = "garnix.server.persistence needs the user 'garnix' to be present in 'users.users', but some other module forced it out.";
+              }
+            ];
+
+            services.openssh.enable = true;
+
+            security.sudo = {
+              enable = true;
+              execWheelOnly = true;
+              wheelNeedsPassword = false;
+            };
+
+            nix.settings.trusted-users = [ "garnix" ];
+
+            users.users.garnix = {
+              description = "A user garnix uses for redeploying ${cfg.name}";
+              isNormalUser = true;
+              createHome = false;
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTPguYAuqp6qCU43u8g2hgWz4MLCEPPyoVPYO53qB+t garnixServer@garnix.io"
+              ];
+              extraGroups = [ "wheel" ];
+            };
+          })
+        ];
       };
 
     lib = rec {
